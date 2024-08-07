d55636f65ddcc90af4e4ef344021d3f660322c93
Alanjunior45 <166314144+Alanjunior45@users.noreply.github.com>
2024-08-07 00:21:40 -0300

    feat: ultiliza o yargs como leitor de argumentos

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Node js para Backende - 1616001
 
+
+Estamos usando no repositorio o commit semântico [semantic commit](https://www.conventionalcommits.org/en/v1.0.0/).
+
 # Links Úteis
 
 [Replit - Simulador de Linguagens](https://replit.com/@CcerofeFeij/Nodejs#index.js)
--- aula01/exemplo1.js
+++ aula01/exemplo1.js
@@ -1,9 +1,9 @@
 const args = process.argv;
 
-console.log(args[1],);
 console.log(args[2],);
-console.log(args[3],);
+//console.log(args[2],);
+//console.log(args[3],);
 
 
-console.log(`Aula de node 1 - argumento: ${args[1]} ${args[2]} ${args[3]}`);
+console.log(`Aula de node 1 - argumento: ${args[2]}`);
 //console.log(`Aula de node 1 - argumento: ${args[3]}`);
