6f8f98e6f74591113b67049b50546209c3a49b80
Alanjunior45 <166314144+Alanjunior45@users.noreply.github.com>
2024-08-25 23:55:06 -0300

    feat: não consegui roddar o codigo

--- aula04/backend/server.js
+++ aula04/backend/server.js
@@ -1,22 +1,37 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-   
-  if(req.url === '/clientes') {
+  console.log(req.method)
+
+  // if(req.url === '/form') {
+  //   res.setHeader('Content-Type', 'text/html');
+  //   const form = '<form action="http://localhost:3000/cadastro" method="post">' +
+  //     '<input type="text" name="cidade" placehouder="Cidade">' +
+  //     '<button type="submit">ENVIAR</button></form>';
+  //   res.end(form);
+  //   return;
+  // }
+
+
+  if (req.url === '/clientes') {
     res.end("Pagina de clientes")
     return;
-   }
+  }
 
-   if(req.url === '/contatos') {
-      res.end("Pagina de contatos");
-      return;
-   }
-   if(req.url === '/cadastros') {
-    res.end("Pagina de contatos");
+  if (req.url === '/contato') {
+    res.end("Pagina de contato");
     return;
- }
+  }
+  
+  if(req.url === '/cadastro' && req.method === 'POST') {
+    req.end("Cadastro efetuado com sucesso!");
+    return;
+  }
+
   res.end("rodando..........");
 
 });
 
-server.listen(3000, 'localhost');
+server.listen(3000, 'localhost', () => {
+  Console.log('Servidor operando...');
+});
